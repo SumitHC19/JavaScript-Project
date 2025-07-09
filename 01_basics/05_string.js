4949f776f0bbcd2d55cc6789f9c3d5212f459133
SumitHC19 <smk436143@gmail.com>
2025-07-09 10:59:34 +0530

    addded string.js

---
+++ 01_basics/05_string.js
@@ -0,0 +1,33 @@
+const name = "sumit"
+const age = 24
+
+// console.log(name + age)
+// console.log(`my name is ${name} and my age is ${age}`)
+
+
+// console.log(name.length)
+// console.log(name.__proto__);
+// console.log(name[2]);
+
+// console.log(name.toUpperCase());
+// console.log(name.charAt(2));
+// console.log(name.indexOf('t'));
+
+
+const newString = name.substring(0, 4)
+console.log(newString);
+
+const anotherString = name.slice(-5, 3)
+console.log(anotherString);
+
+const newStringOne = "   sumit    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('sundar'))
+
+// console.log(name.split('-'));
